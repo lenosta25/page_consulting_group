bc6a86434e6a4f5dd3cdda506a428bfd76472087
Lena <93406015+lenosta25@users.noreply.github.com>
2022-09-10 22:09:17 +0300

    added a section about company with an automatic counter

---
+++ js/animated-counter.js
@@ -0,0 +1,26 @@
+const counters = document.querySelectorAll("#counter");
+
+const loadingObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    // если элемент находится в видимой части браузера,подгружаем
+    if (entry.isIntersecting) {
+      // анимация counter
+      counters.forEach((counter) => {
+        counter.innerHTML = "0";
+        const updateCounter = () => {
+          const target = +counter.getAttribute("data-target");
+          const count = +counter.innerText;
+          if (count < target) {
+            counter.innerText = count + 1;
+            setTimeout(updateCounter, 1);
+          } else {
+            counter.innerText = target;
+          }
+        };
+        updateCounter();
+      });
+    }
+  });
+});
+// указываем, за чем необходимо наблюдать
+loadingObserver.observe(document.querySelector(".about-company"));
